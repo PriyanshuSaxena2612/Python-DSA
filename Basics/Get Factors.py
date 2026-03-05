30c88b60471ac63b309d96b18966f6bf8d529bbe
Priyanshu Saxena <53018678+PriyanshuSaxena2612@users.noreply.github.com>
2026-03-05 16:45:02 +0530

    Added Get Factors.py

---
+++ Basics/Get Factors.py
@@ -0,0 +1,25 @@
+# print all factors of a number
+# 10 -> [1,2,5,10]
+# 15 -> [1,3,5,15]
+# 20 -> [1,2,4,5,10,20]
+
+import math
+def get_factors(num:int)->list:
+    """
+    Returns all factors of a positive number
+    Args:
+        nums(int): Input Number
+    Returns:
+        list[int]: Sorted list of factors
+    """
+    factors = []
+    if num <= 0:
+        raise ValueError("Input must be a positive number")
+    for i in range(1, math.isqrt(num)+1):
+        if num % i == 0:
+            factors.append(i)
+            if i != num // i:
+                factors.append(num//i)
+    return sorted(factors)
+if __name__ == "__main__":
+    print(get_factors(8))
