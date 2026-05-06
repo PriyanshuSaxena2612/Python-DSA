06d59d241691173e1a513a17b9249a4cd50b235e
Priyanshu Saxena <53018678+PriyanshuSaxena2612@users.noreply.github.com>
2026-05-06 23:33:15 +0530

    Create 1861 - Rotating the Box.py

---
+++ LeetCode/1861 - Rotating the Box.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        rows, cols = len(boxGrid), len(boxGrid[0])
+        for r in range(rows):
+            i = cols - 1
+            for c in reversed(range(cols)):
+                if boxGrid[r][c] == '#':
+                    boxGrid[r][c], boxGrid[r][i] = boxGrid[r][i], boxGrid[r][c]
+                    i -= 1
+                elif boxGrid[r][c] == '*':
+                    i = c - 1
+        boxGrid = [list(row) for row in zip(*boxGrid[::-1])]
+        return boxGrid
