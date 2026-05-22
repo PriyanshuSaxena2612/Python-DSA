6d0ae48c137e857ab0f1321eb78cd00fdf61742e
Priyanshu Saxena <53018678+PriyanshuSaxena2612@users.noreply.github.com>
2026-05-23 00:42:53 +0530

    Update 33 - Search in Rotated Sorted Array.py

--- LeetCode/33 - Search in Rotated Sorted Array.py
+++ LeetCode/33 - Search in Rotated Sorted Array.py
@@ -1,15 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        i, mid, j = 0, 0, n-1
-        while i <= j:
-            mid = (i + j) // 2
-            if nums[i] <= nums[mid]: # Left Half is sorted
-                if target == nums[mid]:
-                    return mid
-                elif target < nums[mid] and target >= nums[i]:
-                    j = mid - 1
-                elif target > nums[mid]:
-                    # Check in the other half (might be unsorted)
-        return mid
-        
+        i,j = 0,n-1
+        while i<=j:
+            mid=(i+j)//2
+            # check if mid index is the target
+            if target==nums[mid]:
+                return mid
+            # check if the left array is sorted
+            if nums[i]<=nums[mid]:
+                # check if target is in the left sorted array
+                if target>=nums[i] and target<nums[mid]:
+                    j=mid-1
+                # else check in the second half
+                else:
+                    i=mid+1
+            # else the right half is sorted
+            else:
+                if target<=nums[j] and target>nums[mid]:
+                    i=mid+1
+                # else check in the second half
+                else:
+                    j=mid-1
+        return -1
