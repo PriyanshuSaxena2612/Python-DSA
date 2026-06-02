cfa2bfa1bdd370a1c6c85024f123fc17935eaf2d
Priyanshu Saxena <53018678+PriyanshuSaxena2612@users.noreply.github.com>
2026-06-03 00:17:31 +0530

    Update 3633 - Earliest Finish Time for Land and Water Rides I.py

--- LeetCode/3633 - Earliest Finish Time for Land and Water Rides I.py
+++ LeetCode/3633 - Earliest Finish Time for Land and Water Rides I.py
@@ -2,15 +2,18 @@ class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
         l = len(landStartTime)
         w = len(waterStartTime)
-        sum_, min_ = float('inf'), float('inf')
+        min_ = float('inf')
         for i in range(l):
             for j in range(w):
-                if (landStartTime[i] + landDuration[i]) >= waterStartTime[j]:
-                    sum_ = landStartTime[i] + landDuration[i] + waterDuration[j]
-                if (waterStartTime[j] + waterDuration[j]) >= landStartTime[i]:
-                    sum_ = waterStartTime[j] + waterDuration[j] + landDuration[i]
-                else:
-                    sum_ = landStartTime[i] + landDuration[i] + waterStartTime[j] + waterDuration[j]
-                min_ = min(sum_, min_)
+                land_route = landStartTime[i] + landDuration[i]
+                water_start = max(land_route, waterStartTime[j])
+                total_A = water_start + waterDuration[j]
+
+                water_route = waterStartTime[j] + waterDuration[j]
+                land_start = max(water_route, landStartTime[i])
+                total_B = land_start + landDuration[i]
+
+                min_ = min(min_, total_A, total_B)
+                
         return min_
         
